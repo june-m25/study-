ed0ea471486bd262ab1454fdba98b256fc2b4cbf
june-m25 <matsuminejune@gmail.com>
2022-10-03 22:44:41 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -3,22 +3,31 @@ let list = document.getElementsByClassName("pull-down-list")
 window.addEventListener('load', function(){
   const pullDownButton =  document.getElementById("lists")
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
+    this.setAttribute("style", "background-color:blue;")
   })
 })
 
 window.addEventListener('load', function(){
     const pullDownButton =  document.getElementById("lists")
     pullDownButton.addEventListener('mouseout', function(){
-      pullDownButton.removeAttribute("style", "background-color:red;")
+      this.removeAttribute("style", "background-color:red;")
     })
 })
 
   window.addEventListener('load', function(){
     const pullDownButton =  document.getElementById("lists")
     pullDownButton.addEventListener('click', function(){
-      pullDownButton.setAttribute("style", "background-color:green;")
+      this.setAttribute("style", "background-color:green;")
     })
 })
 
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down")
+pullDownButton.addEventListener('click', function() {
+  if (pullDownParents.getAttribute("style") == "display:block;"){ 
+    pullDownParents.removeAttribute("style", "display:block;")
+  } else {
+    pullDownParents.setAttribute("style", "display:block;")
+  }
+})
   
